d48b5a5569245ca7dbceda00c2928ddf905d4094
Ilham <im615143@gmail.com>
2024-12-07 20:34:27 +0400

    Update configuration

--- nixos/modules/shell/kmscon.nix
+++ nixos/modules/shell/kmscon.nix
@@ -2,5 +2,14 @@
 
 {
   # Better linux console 
-  # services.kmscon.enable = true;
+  services.kmscon = { 
+
+    enable = true;
+
+    hwRender = true;
+
+    # fonts = [ 
+    #   { name = "Source Code Pro"; package = pkgs.source-code-pro; } 
+    # ]; 
+  };
 }

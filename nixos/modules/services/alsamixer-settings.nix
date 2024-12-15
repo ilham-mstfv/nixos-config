6a3d07e20c58c9b8b28128fe986e3bbd07be9d3b
Ilham <im615143@gmail.com>
2024-12-16 01:08:37 +0400

    Update configuration

--- nixos/modules/services/alsamixer-settings.nix
+++ nixos/modules/services/alsamixer-settings.nix
@@ -4,18 +4,22 @@
   ### Alsamixer Stable Settings ###
   ### These settings fix my speakers ###  
 
+  /*
   ## Comment below if on Unstable. ##  
-  # sound.enable = true;
-  # sound.extraConfig = ''
-  #   Saving alsamixer settings
-  #   load-module module-alsa-sink
-  #   load-module module-alsa-source
-  #
-  #   # Disabling Auto-Mute Mode
-  #   options snd-hda-intel model=pch
-  #   options snd-hda-intel auto_mute=0
-  # '';
+  ## Deprecated ##
+  sound.enable = true;
+  sound.extraConfig = ''
+    Saving alsamixer settings
+    load-module module-alsa-sink
+    load-module module-alsa-source
   
+    # Disabling Auto-Mute Mode
+    options snd-hda-intel model=pch
+    options snd-hda-intel auto_mute=0
+  '';
+  */
+ 
+
   ## Comment below if on Stable. ##
   hardware.alsa.enablePersistence = true;
 

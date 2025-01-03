083eb6aa00e97625ca6d26bdef270b98e0a47245
Ilham <im615143@gmail.com>
2025-01-03 17:44:20 +0400

    add lenovo to flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,6 +17,11 @@
         system = "x86_64-linux";
         modules = [ ./hosts/gpd/configuration.nix ];
       };    
+
+      lenovo = nixpkgs.lib.nixosSystem {      
+        system = "x86_64-linux";
+        modules = [ ./hosts/lenovo/configuration.nix ];
+      }; 
     };
 
   };

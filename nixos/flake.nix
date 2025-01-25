b014960628179e5ed62eae5a6e8baae1a11a1f0c
Ilham <im615143@gmail.com>
2025-01-26 00:24:20 +0400

    add home-manager to flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,6 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -10,16 +15,19 @@
     nixosConfigurations = { 
       pc = nixpkgs.lib.nixosSystem {      
         system = "x86_64-linux";      
+	specialArgs = { inherit inputs; };
         modules = [ ./hosts/pc/configuration.nix ];
       };
 
       gpd = nixpkgs.lib.nixosSystem {      
         system = "x86_64-linux";
+	specialArgs = { inherit inputs; };
         modules = [ ./hosts/gpd/configuration.nix ];
       };    
 
       lenovo = nixpkgs.lib.nixosSystem {      
         system = "x86_64-linux";
+	specialArgs = { inherit inputs; };
         modules = [ ./hosts/lenovo/configuration.nix ];
       }; 
     };

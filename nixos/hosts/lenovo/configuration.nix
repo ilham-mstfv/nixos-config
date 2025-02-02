08c0aa64fb300938b04e0cc700c0c8657576be6b
Ilham <im615143@gmail.com>
2025-02-02 18:15:17 +0400

    remove useless comments in hosts/lenovo/configuration.nix

--- nixos/hosts/lenovo/configuration.nix
+++ nixos/hosts/lenovo/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, inputs, system, vars, ... }:
 
 {
@@ -112,19 +108,12 @@
 
 
   ## Networking ##
-  networking.hostName = "lenovo"; # Define your hostname.
+  networking.hostName = "lenovo";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
 
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  ## Don't change this value ##
+  system.stateVersion = "24.11";
 }

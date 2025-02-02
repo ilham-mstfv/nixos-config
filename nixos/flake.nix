9f9f508e63c5a103106cf69f06fcccb8de7a9bcf
Ilham <im615143@gmail.com>
2025-02-02 14:28:03 +0400

    add some lines in flake.nix: inherit system; and pkgs line in vars

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -13,6 +13,7 @@
   outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
     vars = {
       user = "ilham";
     };
@@ -20,16 +21,19 @@
   {
     nixosConfigurations = { 
       pc = nixpkgs.lib.nixosSystem {      
+        inherit system;
         specialArgs = { inherit self inputs system vars; }; 
         modules = [ ./hosts/pc/configuration.nix ];
       };
 
       gpd = nixpkgs.lib.nixosSystem {      
+        inherit system;
         specialArgs = { inherit self inputs system vars; }; 
         modules = [ ./hosts/gpd/configuration.nix ];
       };    
 
       lenovo = nixpkgs.lib.nixosSystem {      
+        inherit system;
         specialArgs = { inherit self inputs system vars; }; 
         modules = [ ./hosts/lenovo/configuration.nix ];
       }; 

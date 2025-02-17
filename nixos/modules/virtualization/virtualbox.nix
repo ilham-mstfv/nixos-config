146ca0b7bb7da9a21558cd5040e8461a0d44985f
Ilham <im615143@gmail.com>
2025-02-17 23:03:15 +0400

    refactor virtualbox.nix, make it prettier

--- nixos/modules/virtualization/virtualbox.nix
+++ nixos/modules/virtualization/virtualbox.nix
@@ -2,15 +2,22 @@
 
 {
   ### VirtualBox ###
-  virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "${vars.user}" ];
-  
-
-  ## Enable VBox Quest Additions ##
-  # virtualisation.virtualbox.host.enableExtensionPack = true;
-  # virtualisation.virtualbox.guest.enable = true;
-  # virtualisation.virtualbox.guest.dragAndDrop = true;
-  # virtualisation.virtualbox.guest.clipboard = true;
-  # virtualisation.virtualbox.guest.seamless = true;
-  # virtualisation.virtualbox.host.enableKvm = true;
+  nixpkgs.config.allowUnfree = true;
+ 
+  virtualisation = { 
+    virtualbox = {
+      host = {
+        enable = true; 
+        enableExtensionPack = true;
+        enableKvm = false; # Experimental
+      };
+      guest = {
+        enable = true;
+        dragAndDrop = true;
+        clipboard = true;
+        seamless = true;
+      };
+    };
+  };
 }

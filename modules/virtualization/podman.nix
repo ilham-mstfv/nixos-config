54991ce6745d814ce53bd5de7b05ecbea2e38268
Ilham <im615143@gmail.com>
2025-06-07 01:31:03 +0400

    Create podman.nix module

---
+++ modules/virtualization/podman.nix
@@ -0,0 +1,22 @@
+{ pkgs, libs, vars, ... }:
+
+{
+  ### Podman ###
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  ## Write here your username ##
+  users.extraGroups.podman.members = [ "${vars.user}" ];   
+
+  ## Install some packages ##
+  environment.systemPackages = with pkgs; [ 
+    podman podman-tui podman-compose
+    dive
+  ];
+}
